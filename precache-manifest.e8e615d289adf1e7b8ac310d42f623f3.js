40fed81283c931caf052b11d0162620ca4635019
SR <sathish.sr24@gmail.com>
2019-07-31 12:33:44 +0530

    Updates

---
+++ precache-manifest.e8e615d289adf1e7b8ac310d42f623f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d47d1488aa6d2a3d662cfb35533318e",
+    "url": "/terminator/index.html"
+  },
+  {
+    "revision": "24a91ae13c619a819173",
+    "url": "/terminator/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/terminator/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "24a91ae13c619a819173",
+    "url": "/terminator/static/js/main.1201b2a5.chunk.js"
+  },
+  {
+    "revision": "da2225ba5279d04ad48d",
+    "url": "/terminator/static/js/runtime~main.2e11ea5b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/terminator/static/media/logo.5d5d9eef.svg"
+  }
+]);
